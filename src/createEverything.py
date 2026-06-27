967768b2c57d1f6f8e11dc3faa87a04b8f9fe125
Daveberry <84567826+daveberrys@users.noreply.github.com>
2026-06-27 23:55:01 +0800

    remove extra `/` for path

--- src/createEverything.py
+++ src/createEverything.py
@@ -18,20 +18,20 @@ def main(
     pathToLogo,
     pathToBanner
 ):
-    if os.path.exists(f"{pathToExport}/.solar-engine"):
+    if os.path.exists(f"{pathToExport}.solar-engine"):
         pathExists = q.getBoolean("`.solar-engine` exists in this path. Do you wish to delete it?")
         if pathExists == "y" or pathExists == "Y":
-            shutil.rmtree(f"{pathToExport}/.solar-engine")
+            shutil.rmtree(f"{pathToExport}.solar-engine")
             pass
         else:
             print("Exiting...")
             sys.exit(1)
     else:
-        os.mkdir(f"{pathToExport}/.solar-engine")
+        os.mkdir(f"{pathToExport}.solar-engine")
 
         # config.json // readme.md
         # this is writing
-        with open(f"{pathToExport}/.solar-engine/config.json", "w") as f:
+        with open(f"{pathToExport}.solar-engine/config.json", "w") as f:
             f.write(f"""{{
 "isOpenSource": "{soq.getBoolean(isOpenSource)}",
 "canMessWithComputer": "{soq.getBoolean(canMessWithComputer)}",
@@ -45,10 +45,10 @@ def main(
 "madeByURL": "{madeByURL}",
 "downloadURL": "{downloadURL}"
 }}""")
-        with open(f"{pathToExport}/.solar-engine/readme.md", "w") as f:
+        with open(f"{pathToExport}.solar-engine/readme.md", "w") as f:
             f.write(f"A mod about {title}")
 
         # logo.png // banner.png
         # this is copying
-        shutil.copyfile(pathToLogo, f"{pathToExport}/.solar-engine/logo.png")
-        shutil.copyfile(pathToBanner, f"{pathToExport}/.solar-engine/banner.png")
+        shutil.copyfile(pathToLogo, f"{pathToExport}.solar-engine/logo.png")
+        shutil.copyfile(pathToBanner, f"{pathToExport}.solar-engine/banner.png")
