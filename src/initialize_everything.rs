a13a620b79b25a0cdab7fa35710d7837224c6a83
daveberrys <84567826+daveberrys@users.noreply.github.com>
2026-07-04 18:57:54 +0800

    added type key for config

--- src/initialize_everything.rs
+++ src/initialize_everything.rs
@@ -7,11 +7,12 @@ const CONTENT_README: &str = "# A mod about {title}";
 const CONTENT_JSON_CONFIG: &str = r#"{
     "isOpenSource": "{is_open_source}",
     "canMessWithComputer": "{can_mess_with_computer}",
-    
+    "type": "{type}",
+
     "title": "{title}",
     "description": "{description}",
     "madeBy": "{made_by}",
-    
+
     "downloadURL": "{download_url}",
     "externalURL": "{external_url}",
     "githubURL": "{github_url}",
@@ -21,6 +22,7 @@ const CONTENT_JSON_CONFIG: &str = r#"{
 pub fn init_yes(
     path_to_logo: &str,
     path_to_banner: &str,
+    what_type: Vec<i32>,
     is_open_source: &str,
     can_mess_with_computer: &str,
     title: &str,
@@ -35,7 +37,7 @@ pub fn init_yes(
     // === === === config.json // readme.md === === ===
     let formatted_string = format!("{}{}", path_to_export, ".solar-engine");
     let extended_path = Path::new(&formatted_string);
-    
+
     if extended_path.exists() {
         let path_exists_prompt = boolean(&format!("`{}` {}", path_to_export.bright_black(), "path exists. Do you wish to delete it?".yellow()));
 
@@ -49,6 +51,15 @@ pub fn init_yes(
         fs::create_dir(extended_path).unwrap();
     }
 
+    let mut returned_type_shi: &str = "Other";
+    for num in what_type {
+        if num == 1 {
+            returned_type_shi = "Scripts";
+        } else if num == 2 {
+            returned_type_shi = "Mods";
+        }
+    }
+
     let populated_json = CONTENT_JSON_CONFIG
         .replace("{is_open_source}", is_open_source)
         .replace("{can_mess_with_computer}", can_mess_with_computer)
@@ -58,11 +69,12 @@ pub fn init_yes(
         .replace("{download_url}", download_url)
         .replace("{external_url}", external_url)
         .replace("{github_url}", github_url)
-        .replace("{made_by_url}", made_by_url);
- 
+        .replace("{made_by_url}", made_by_url)
+        .replace("{type}", returned_type_shi);
+
     let formatted_readme = CONTENT_README
         .replace("{title}",  title);
-    
+
     create_file(&format!("{}/readme.md", formatted_string), &formatted_readme);
     create_file(&format!("{}/config.json", formatted_string), &populated_json);
 
